95ead1f578efc0dd472dd5b8da04c4ba3e10d7dc
yangjl <jolyang@ucdavis.edu>
2016-02-27 19:35:59 -0800

    add lots of data

---
+++ slurm-scripts/run_hm34.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH -D /home/jolyang/Documents/Github/N2
+#SBATCH -o /home/jolyang/Documents/Github/N2/slurm-log/testout-%j.txt
+#SBATCH -e /home/jolyang/Documents/Github/N2/slurm-log/err-%j.txt
+#SBATCH -J run_hm34.sh
+#SBATCH--mail-user=yangjl0930@gmail.com
+#SBATCH--mail-type=END
+#SBATCH--mail-type=FAIL #email if fails
+set -e
+set -u
+
+cd largedata/hapmixrun
+perl bin/runHapmix.pl hprun_gen1610_chr1.par
+perl bin/runHapmix.pl hprun_gen1610_chr2.par
+perl bin/runHapmix.pl hprun_gen1610_chr3.par
+perl bin/runHapmix.pl hprun_gen1610_chr4.par
+perl bin/runHapmix.pl hprun_gen1610_chr5.par
+perl bin/runHapmix.pl hprun_gen1610_chr6.par
+perl bin/runHapmix.pl hprun_gen1610_chr7.par
+perl bin/runHapmix.pl hprun_gen1610_chr8.par
+perl bin/runHapmix.pl hprun_gen1610_chr9.par
+perl bin/runHapmix.pl hprun_gen1610_chr10.par
